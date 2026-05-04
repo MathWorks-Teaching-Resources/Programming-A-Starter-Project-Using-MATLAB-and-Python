320f599c9e0382a2766535e3bc609feb0d74bd6a
Emma Smith Zbarsky <esmithzb@mathworks.com>
2026-05-04 17:31:43 -0400

    update: Simplify and improve tests

--- SoftwareTests/PreFiles/PreCheckingTheWeatherSoln.m
+++ SoftwareTests/PreFiles/PreCheckingTheWeatherSoln.m
@@ -3,21 +3,22 @@
 KnownIssuesID = "";
 % ---- Pre-run commands -----
  
-try 
-copyfile(fullfile(currentProject().RootFolder,"InternalFiles","Solutions","myAPIkey.txt"),...
-    fullfile(currentProject().RootFolder,"Scripts","myAPIkey.txt"))
-catch
-    copyfile("Response.json",fullfile(currentProject().RootFolder,"InstructorResources","Solutions","Response.json"))
-    pycode = ["import checkcurrentweather"
-    "import json"
-    ""
-    "json_data = json.load(open(""Response.json""))"
-    "currentWeather = checkcurrentweather.parse_current_json(json_data)"
-    ];
-    apikey = "TestString";
-    pyrun = @(txt,out,varargin)TestPyRun(pycode,out,varargin);
-end
-
-function out = TestPyRun(txt,in,varargin)
-out = pyrun(txt,in);
-end
+% try 
+% copyfile(fullfile(currentProject().RootFolder,"InternalFiles","Solutions","myAPIkey.txt"),...
+%     fullfile(currentProject().RootFolder,"Scripts","myAPIkey.txt"))
+% catch
+%     disp("No API key found.")
+%     % copyfile("Response.json",fullfile(currentProject().RootFolder,"InstructorResources","Solutions","Response.json"))
+%     % pycode = ["import checkcurrentweather"
+%     % "import json"
+%     % ""
+%     % "json_data = json.load(open(""Response.json""))"
+%     % "currentWeather = checkcurrentweather.parse_current_json(json_data)"
+%     % ];
+%     apikey = "TestString";
+%     % pyrun = @(txt,out,varargin)TestPyRun(pycode,out,varargin);
+% end
+% 
+% function out = TestPyRun(txt,in,varargin)
+% out = pyrun(txt,in);
+% end
